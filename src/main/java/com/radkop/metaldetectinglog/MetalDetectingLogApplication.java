caabf06fb25671a57dc49d95284c68bd7838654b
Radoslaw Kopko <radoslaw.kopko@pwc.com>
2019-12-18 12:18:44 +0100

    Created project using Spring Initializr

---
+++ src/main/java/com/radkop/metaldetectinglog/MetalDetectingLogApplication.java
@@ -0,0 +1,13 @@
+package com.radkop.metaldetectinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalDetectingLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalDetectingLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radkop/metaldetectinglog/MetalDetectingLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radkop.metaldetectinglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalDetectingLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
